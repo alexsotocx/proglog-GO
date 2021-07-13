7c237738d34d927a5774f9ef3774b5c64ac89beb
Alexander Soto <asoto@minddoc.de>
2021-07-13 20:50:11 +0200

    distributed log

--- internal/log/config.go
+++ internal/log/config.go
@@ -1,6 +1,15 @@
 package log
 
+import (
+	"github.com/hashicorp/raft"
+)
+
 type Config struct {
+	Raft struct {
+		raft.Config
+		StreamLayer raft.StreamLayer
+		Boostrap    bool
+	}
 	Segment struct {
 		MaxStoreBytes uint64
 		MaxIndexBytes uint64
