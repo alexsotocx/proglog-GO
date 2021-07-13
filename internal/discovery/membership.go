3d2bd29486872deba3771ba4fd7d79336958a178
Alexander Soto <asoto@minddoc.de>
2021-07-13 08:00:23 +0200

    add serf service discovery

--- internal/discovery/membership.go
+++ internal/discovery/membership.go
@@ -1,23 +1,13 @@
+// START: start
 package discovery
 
 import (
 	"net"
 
-	"github.com/hashicorp/serf/serf"
 	"go.uber.org/zap"
+
+	"github.com/hashicorp/serf/serf"
 )
-
-type Config struct {
-	NodeName       string
-	BinAddr        string
-	Tags           map[string]string
-	StartJoinAddrs []string
-}
-
-type Handler interface {
-	join(name, addr string) error
-	Leave(name string) error
-}
 
 type Membership struct {
 	Config
@@ -39,8 +29,17 @@ func New(handler Handler, config Config) (*Membership, error) {
 	return c, nil
 }
 
-func (m *Membership) setupSerf() error {
-	addr, err := net.ResolveTCPAddr("tcp", m.BinAddr)
+// END: start
+// START: setup
+type Config struct {
+	NodeName       string
+	BindAddr       string
+	Tags           map[string]string
+	StartJoinAddrs []string
+}
+
+func (m *Membership) setupSerf() (err error) {
+	addr, err := net.ResolveTCPAddr("tcp", m.BindAddr)
 	if err != nil {
 		return err
 	}
@@ -49,35 +48,106 @@ func (m *Membership) setupSerf() error {
 	config.MemberlistConfig.BindAddr = addr.IP.String()
 	config.MemberlistConfig.BindPort = addr.Port
 	m.events = make(chan serf.Event)
+	config.EventCh = m.events
 	config.Tags = m.Tags
 	config.NodeName = m.Config.NodeName
 	m.serf, err = serf.Create(config)
 	if err != nil {
 		return err
 	}
-	go m.eventHandler()
+	go m.eventHandler() //<label id="handlergoroutine" />
 	if m.StartJoinAddrs != nil {
 		_, err = m.serf.Join(m.StartJoinAddrs, true)
 		if err != nil {
 			return err
 		}
 	}
-
 	return nil
 }
 
+// END: setup
+
+// START: handler
+type Handler interface {
+	Join(name, addr string) error
+	Leave(name string) error
+}
+
+// END: handler
+
+// START: event_handler
 func (m *Membership) eventHandler() {
 	for e := range m.events {
 		switch e.EventType() {
 		case serf.EventMemberJoin:
-			{
-				for _, member := range e.(serf.MemberEvent).Members {
-					if m.isLocal(member) {
-						continue
-					}
-					m.handleJoin(member)
+			for _, member := range e.(serf.MemberEvent).Members {
+				if m.isLocal(member) {
+					continue
 				}
+				m.handleJoin(member)
+			}
+		case serf.EventMemberLeave, serf.EventMemberFailed:
+			for _, member := range e.(serf.MemberEvent).Members {
+				if m.isLocal(member) {
+					return
+				}
+				m.handleLeave(member)
 			}
 		}
 	}
 }
+
+func (m *Membership) handleJoin(member serf.Member) {
+	if err := m.handler.Join(
+		member.Name,
+		member.Tags["rpc_addr"],
+	); err != nil {
+		m.logError(err, "failed to join", member)
+	}
+}
+
+func (m *Membership) handleLeave(member serf.Member) {
+	if err := m.handler.Leave(
+		member.Name,
+	); err != nil {
+		m.logError(err, "failed to leave", member)
+	}
+}
+
+// END: event_handler
+
+// START: rest
+func (m *Membership) isLocal(member serf.Member) bool {
+	return m.serf.LocalMember().Name == member.Name
+}
+
+func (m *Membership) Members() []serf.Member {
+	return m.serf.Members()
+}
+
+func (m *Membership) Leave() error {
+	return m.serf.Leave()
+}
+
+// END: rest
+
+// START: log_error
+// func (m *Membership) logError(err error, msg string, member serf.Member) {
+// 	log := m.logger.Error
+// 	if err == raft.ErrNotLeader {
+// 		log = m.logger.Debug
+// 	}
+// 	log(
+// 		msg,
+// 		zap.Error(err),
+// 		zap.String("name", member.Name),
+// 		zap.String("rpc_addr", member.Tags["rpc_addr"]),
+// 	)
+// }
+func (m *Membership) logError(err error, msg string, member serf.Member) {
+	m.logger.Error(msg, zap.Error(err), zap.String("name", member.Name), zap.String("rpd_addr", member.Tags["rpc_addr"]))
+}
+
+// END: log_error
+
+// END: rest
